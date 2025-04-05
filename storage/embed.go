7f7ae6a1473a3a7f687ab052670fee277f25bdfd
MegaGrindStone <megagrindstone@gmail.com>
2025-04-06 01:43:16 +0700

    Satisfy linter

--- storage/embed.go
+++ storage/embed.go
@@ -2,4 +2,5 @@ package storage
 
 import "context"
 
+// EmbeddingFunc is a function type for embedding text into a vector.
 type EmbeddingFunc func(ctx context.Context, text string) ([]float32, error)
